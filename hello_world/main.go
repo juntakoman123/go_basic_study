5495d4876303ae8bd9ef1b707309ef3a4a2d704b
junta <juntakoman123@gmail.com>
2024-07-08 00:19:58 +0900

    hello関数実装完了

--- hello_world/main.go
+++ hello_world/main.go
@@ -2,12 +2,6 @@ package main
 
 import "fmt"
 
-const englishHelloPrefix = "Hello, "
-
-func Hello(name string) string {
-	return englishHelloPrefix + name
-}
-
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("", ""))
 }
