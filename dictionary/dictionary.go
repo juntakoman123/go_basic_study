1802aa3aae3575d1d2dff35776a4230061459055
katakawaj <juntakoman123@gmail.com>
2024-07-08 18:02:51 +0900

    辞書構造体 途中まで実装

---
+++ dictionary/dictionary.go
@@ -0,0 +1,44 @@
+package dictionary
+
+type Dictionary map[string]string
+
+const (
+	ErrNotFound         = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists       = DictionaryErr("cannot add word because it already exists")
+	ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
+}
+
+func (d Dictionary) Add(word, definition string) error {
+	_, ok := d[word]
+	if ok {
+		return ErrWordExists
+	}
+
+	d[word] = definition
+	return nil
+}
+
+func (d Dictionary) Update(word, newDefinition string) error {
+
+	_, ok := d[word]
+	if !ok {
+		return ErrWordDoesNotExist
+	}
+
+	d[word] = newDefinition
+	return nil
+
+}
