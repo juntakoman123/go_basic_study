c298e19d0a942bda7b34d78f75df6ea6753a1e90
junta <juntakoman123@gmail.com>
2024-11-12 02:15:33 +0900

    ローマ数字への変換関数途中まで

---
+++ roman-numerals/numeral.go
@@ -0,0 +1,22 @@
+package numeral
+
+import "strings"
+
+func ConvertToRoman(arabic int) string {
+
+	var result strings.Builder
+
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+		result.WriteString("I")
+	}
+
+	return result.String()
+}
